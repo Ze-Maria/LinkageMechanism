8b9cf6419c2a1609a90ca01098d926c33882bfdb
Ze-Maria <66984420+Ze-Maria@users.noreply.github.com>
2021-03-19 15:27:29 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'LinkageMechanism',
+    version = '1.0.0',
+    author = 'José Maria Bezerra',
+    author_email = 'zemaria@ufpe.br',
+    packages = ['LinkageMechanism'],
+    project_urls = {
+        'Código fonte': 'LinkageMechanism',
+        'Download': 'LinkageMechanism/archive/1.0.0.zip'
+    },
+    license = 'Mozilla Public License 2.0',
+    description = 'App para análise de Mecanismos',
+    keywords = 'mechanism linkage ufpe'
+)
