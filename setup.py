4b9311620dcf52cc2593dc0e0d35391633a90b0b
Ze-Maria <66984420+Ze-Maria@users.noreply.github.com>
2021-03-19 18:26:20 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup
 
 setup(
     name = 'LinkageMechanism',
-    version = '1.0.0',
+    version = '1.0.1',
     author = 'José Maria Bezerra',
     author_email = 'zemaria@ufpe.br',
     packages = ['LinkageMechanism'],
     url = 'https://https://github.com/Ze-Maria/LinkageMechanism',
     project_urls = {
-        'Código fonte': 'LinkageMechanism',
-        'Download': 'https://https://github.com/Ze-Maria/LinkageMechanism/archive/1.0.0.zip'
+        'Código fonte': 'https://github.com/Ze-Maria/LinkageMechanism',
+        'Download': 'https://github.com/Ze-Maria/LinkageMechanism/blob/main/LinkageMechanism.py'
     },
     license = 'MIT License',
     description = 'App para análise de Mecanismos',
     keywords = 'mechanism linkage ufpe'
 )
+#https://www.alura.com.br/artigos/como-publicar-seu-codigo-python-no-pypi
+# Esta, acima, é a página que ensina a colocar o projeto no PyPi.
